77f446bdafc7057d522f9723dc7b6c7f55bf8bc5
Drzaln <drzaln@null.net>
2021-06-11 22:39:53 +0700

    adding gojek bottom nav - progress

--- src/screenList/index.js
+++ src/screenList/index.js
@@ -1,6 +1,7 @@
 import AnimateButton from '../screens/AnimatedButton/AnimateButton'
 import AnimatedTextInput from '../screens/AnimatedTextInput/AnimatedTextInput'
 import CheckButton from '../screens/CheckButton'
+import GojekBottomNav from '../screens/GojekBottomNav'
 import AnimatedRadioButton from '../screens/RadioButton/AnimatedRadioButton'
 import SwitchButton from '../screens/SwitchButton'
 
@@ -9,7 +10,8 @@ const SCREENS_LIST = [
 	{ screen: AnimatedTextInput, screenRoute: 'AnimatedTextInput', name: 'Animated Text Input' },
 	{ screen: AnimatedRadioButton, screenRoute: 'AnimatedRadioButton', name: 'Animated Radio Button' },
 	{ screen: SwitchButton, screenRoute: 'SwitchButton', name: 'Animated Switch Button' },
-	{ screen: CheckButton, screenRoute: 'CheckButton', name: 'Animated Check Button' }
+	{ screen: CheckButton, screenRoute: 'CheckButton', name: 'Animated Check Button' },
+	{ screen: GojekBottomNav, screenRoute: 'GojekBottomNav', name: 'Gojek Bottom Navigation' }
 ]
 
 export default SCREENS_LIST
