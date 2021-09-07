ae0a507538f64170aaa11075215b37dc572539d8
Drzaln <doddyrizaln@gmail.com>
2021-09-07 11:32:46 +0700

    feat: upgrade to rn65, reanimated 2.3.0-alpha.3

--- src/screens/RadioButton/components/RadioButton.js
+++ src/screens/RadioButton/components/RadioButton.js
@@ -1,91 +1,111 @@
-import React from 'react'
-import { Pressable, StyleSheet, Text } from 'react-native'
-import Animated, { useAnimatedStyle, useSharedValue, withDelay, withTiming } from 'react-native-reanimated'
+import React from 'react';
+import {Pressable, StyleSheet, Text} from 'react-native';
+import Animated, {
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from 'react-native-reanimated';
 
-const RadioButton = ({ title, color = '#8ecae6' }) => {
-	const radioState = useSharedValue(0)
+const RadioButton = ({title, color = '#8ecae6'}) => {
+  const radioState = useSharedValue(0);
+  const colors = ['#ced4da', color];
 
-	const animatedContainer = useAnimatedStyle(() => {
-		const borderColor = radioState.value ? color : '#ced4da'
-		return {
-			borderWidth: radioState.value ? withTiming(15) : withDelay(300, withTiming(2)),
-			borderColor: withTiming(borderColor)
-		}
-	})
-	const firstAnimate = useAnimatedStyle(() => {
-		return {
-			transform: [
-				{ translateY: radioState.value ? withDelay(400, withTiming(0)) : withDelay(100, withTiming(-30)) }
-			]
-		}
-	})
-	const secondAnimate = useAnimatedStyle(() => {
-		return {
-			transform: [
-				{ translateY: radioState.value ? withDelay(450, withTiming(0)) : withDelay(150, withTiming(-30)) }
-			]
-		}
-	})
-	const thirdAnimate = useAnimatedStyle(() => {
-		return {
-			transform: [
-				{ translateY: radioState.value ? withDelay(485, withTiming(0)) : withDelay(185, withTiming(-30)) }
-			]
-		}
-	})
+  const animatedContainer = useAnimatedStyle(() => {
+    return {
+      borderWidth: radioState.value
+        ? withTiming(15)
+        : withDelay(300, withTiming(2)),
+      borderColor: interpolateColor(radioState.value, [0, 1], colors),
+    };
+  });
+  const firstAnimate = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: radioState.value
+            ? withDelay(400, withTiming(0))
+            : withDelay(100, withTiming(-30)),
+        },
+      ],
+    };
+  });
+  const secondAnimate = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: radioState.value
+            ? withDelay(450, withTiming(0))
+            : withDelay(150, withTiming(-30)),
+        },
+      ],
+    };
+  });
+  const thirdAnimate = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: radioState.value
+            ? withDelay(485, withTiming(0))
+            : withDelay(185, withTiming(-30)),
+        },
+      ],
+    };
+  });
 
-	const radioHandler = () => {
-		radioState.value = !radioState.value
-	}
-	return (
-		<Pressable style={styles.pressable} onPress={radioHandler}>
-			<Animated.View style={[ styles.container, animatedContainer ]}>
-				<Animated.View style={[ styles.firstCircle, firstAnimate ]} />
-				<Animated.View style={[ styles.secondCircle, secondAnimate ]} />
-				<Animated.View style={[ styles.thirdCircle, thirdAnimate ]} />
-			</Animated.View>
-			{title ? <Text style={styles.titleText}>{title}</Text> : null}
-		</Pressable>
-	)
-}
+  const radioHandler = () => {
+    radioState.value = !radioState.value;
+  };
+  return (
+    <Pressable style={styles.pressable} onPress={radioHandler}>
+      <Animated.View style={[styles.container, animatedContainer]}>
+        <Animated.View style={[styles.firstCircle, firstAnimate]} />
+        <Animated.View style={[styles.secondCircle, secondAnimate]} />
+        <Animated.View style={[styles.thirdCircle, thirdAnimate]} />
+      </Animated.View>
+      {title ? <Text style={styles.titleText}>{title}</Text> : null}
+    </Pressable>
+  );
+};
 
-export default RadioButton
+export default RadioButton;
 
 const styles = StyleSheet.create({
-	pressable: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		overflow: 'hidden'
-	},
-	titleText: {
-		marginLeft: 8,
-		fontSize: 14
-	},
-	container: {
-		height: 30,
-		width: 30,
-		borderRadius: 100,
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	firstCircle: {
-		backgroundColor: 'white',
-		height: 18,
-		width: 18,
-		borderRadius: 100
-	},
-	secondCircle: {
-		backgroundColor: 'white',
-		height: 14,
-		width: 14,
-		borderRadius: 100,
-		position: 'absolute'
-	},
-	thirdCircle: {
-		backgroundColor: 'white',
-		height: 8,
-		width: 8,
-		borderRadius: 100,
-		position: 'absolute'
-	}
-})
+  pressable: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  titleText: {
+    marginLeft: 8,
+    fontSize: 14,
+  },
+  container: {
+    height: 30,
+    width: 30,
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  firstCircle: {
+    backgroundColor: 'white',
+    height: 18,
+    width: 18,
+    borderRadius: 100,
+  },
+  secondCircle: {
+    backgroundColor: 'white',
+    height: 14,
+    width: 14,
+    borderRadius: 100,
+    position: 'absolute',
+  },
+  thirdCircle: {
+    backgroundColor: 'white',
+    height: 8,
+    width: 8,
+    borderRadius: 100,
+    position: 'absolute',
+  },
+});
--- src/screens/SwitchButton/components/LiquidSwitch.js
+++ src/screens/SwitchButton/components/LiquidSwitch.js
@@ -1,97 +1,122 @@
-import React from 'react'
-import { Pressable, StyleSheet, Text } from 'react-native'
-import Animated, { useAnimatedStyle, useSharedValue, withDelay, withTiming } from 'react-native-reanimated'
+import React from 'react';
+import {Pressable, StyleSheet, Text} from 'react-native';
+import Animated, {
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from 'react-native-reanimated';
 
-const LiquidSwitch = ({ title, activeColor = '#00bbf9', inactiveColor = '#adb5bd' }) => {
-	const switchState = useSharedValue(0)
+const LiquidSwitch = ({
+  title,
+  activeColor = '#00bbf9',
+  inactiveColor = '#adb5bd',
+}) => {
+  const switchState = useSharedValue(0);
 
-	const animatedContainer = useAnimatedStyle(() => {
-		const backgroundColor = switchState.value ? activeColor : inactiveColor
-		return {
-			backgroundColor: withTiming(backgroundColor)
-		}
-	})
-	const firstAnimate = useAnimatedStyle(() => {
-		return {
-			transform: [
-				{ translateX: switchState.value ? withDelay(0, withTiming(25)) : withDelay(0, withTiming(0)) }
-			]
-		}
-	})
-	const secondAnimate = useAnimatedStyle(() => {
-		return {
-			transform: [
-				{ translateX: switchState.value ? withDelay(90, withTiming(33)) : withDelay(90, withTiming(8)) }
-			]
-		}
-	})
-	const thirdAnimate = useAnimatedStyle(() => {
-		return {
-			transform: [
-				{ translateX: switchState.value ? withDelay(145, withTiming(36)) : withDelay(145, withTiming(11)) }
-			]
-		}
-	})
+  const animatedContainer = useAnimatedStyle(() => {
+    return {
+      backgroundColor: interpolateColor(
+        switchState.value,
+        [0, 1],
+        [inactiveColor, activeColor],
+      ),
+    };
+  });
+  const firstAnimate = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: switchState.value
+            ? withDelay(0, withTiming(25))
+            : withDelay(0, withTiming(0)),
+        },
+      ],
+    };
+  });
+  const secondAnimate = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: switchState.value
+            ? withDelay(90, withTiming(33))
+            : withDelay(90, withTiming(8)),
+        },
+      ],
+    };
+  });
+  const thirdAnimate = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: switchState.value
+            ? withDelay(145, withTiming(36))
+            : withDelay(145, withTiming(11)),
+        },
+      ],
+    };
+  });
 
-	const switchHandler = () => {
-		switchState.value = !switchState.value
-	}
+  const switchHandler = () => {
+    switchState.value = withTiming(!switchState.value, {duration: 200});
+  };
 
-	return (
-		<Pressable style={styles.pressable} onPress={switchHandler}>
-			<Animated.View style={[ styles.container, animatedContainer ]}>
-				<Animated.View style={[ styles.secondLiquid, thirdAnimate ]} />
-				<Animated.View style={[ styles.firstLiquid, secondAnimate ]} />
-				<Animated.View style={[ styles.indicator, firstAnimate ]} />
-			</Animated.View>
-			{title ? <Text style={styles.titleText}>{title}</Text> : null}
-		</Pressable>
-	)
-}
+  return (
+    <Pressable style={styles.pressable} onPress={switchHandler}>
+      <Animated.View style={[styles.container, animatedContainer]}>
+        <Animated.View style={[styles.secondLiquid, thirdAnimate]} />
+        <Animated.View style={[styles.firstLiquid, secondAnimate]} />
+        <Animated.View style={[styles.indicator, firstAnimate]} />
+      </Animated.View>
+      {title ? <Text style={styles.titleText}>{title}</Text> : null}
+    </Pressable>
+  );
+};
 
-export default LiquidSwitch
+export default LiquidSwitch;
 
 const styles = StyleSheet.create({
-	pressable: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		overflow: 'hidden'
-	},
-	titleText: {
-		marginLeft: 8,
-		fontSize: 14
-	},
-	container: {
-		height: 30,
-		width: 56,
-		borderRadius: 100,
-		padding: 5,
-		flexDirection: 'row',
-		alignItems: 'center'
-	},
-	indicator: {
-		width: '44%',
-		height: '100%',
-		borderRadius: 100,
-		backgroundColor: '#ffffff'
-		// transform: [ { translateX: 25 } ]
-	},
-	firstLiquid: {
-		width: 14,
-		height: 14,
-		borderRadius: 100,
-		backgroundColor: '#ffffff',
-		position: 'absolute'
-		// transform: [ { translateX: 33 } ]
-		// transform: [ { translateX: 8 } ]
-	},
-	secondLiquid: {
-		width: 8,
-		height: 8,
-		borderRadius: 100,
-		backgroundColor: '#ffffff',
-		position: 'absolute'
-		// transform: [ { translateX: 36 } ]
-		// transform: [ { translateX: 11 } ]
-	}
-})
+  pressable: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  titleText: {
+    marginLeft: 8,
+    fontSize: 14,
+  },
+  container: {
+    height: 30,
+    width: 56,
+    borderRadius: 100,
+    padding: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  indicator: {
+    width: '44%',
+    height: '100%',
+    borderRadius: 100,
+    backgroundColor: '#ffffff',
+    // transform: [ { translateX: 25 } ]
+  },
+  firstLiquid: {
+    width: 14,
+    height: 14,
+    borderRadius: 100,
+    backgroundColor: '#ffffff',
+    position: 'absolute',
+    // transform: [ { translateX: 33 } ]
+    // transform: [ { translateX: 8 } ]
+  },
+  secondLiquid: {
+    width: 8,
+    height: 8,
+    borderRadius: 100,
+    backgroundColor: '#ffffff',
+    position: 'absolute',
+    // transform: [ { translateX: 36 } ]
+    // transform: [ { translateX: 11 } ]
+  },
+});
