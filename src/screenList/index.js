984517fa06a602c815493d019e782094b0cbb3f3
Drzaln <doddyrizaln@gmail.com>
2021-09-18 17:41:46 +0700

    feat: fore coffee menu, still buggy

--- src/screenList/index.js
+++ src/screenList/index.js
@@ -1,23 +1,45 @@
-import AnimateButton from '../screens/AnimatedButton/AnimateButton'
-import AnimatedTextInput from '../screens/AnimatedTextInput/AnimatedTextInput'
-import CheckButton from '../screens/CheckButton'
-import GojekBottomNav from '../screens/GojekBottomNav'
-import MenuSection from '../screens/MenuSection'
-import AnimatedRadioButton from '../screens/RadioButton/AnimatedRadioButton'
-import Slider from '../screens/Slider'
-import CompareImage from '../screens/SlidetoCompare'
-import SwitchButton from '../screens/SwitchButton'
+import AnimateButton from '../screens/AnimatedButton/AnimateButton';
+import AnimatedTextInput from '../screens/AnimatedTextInput/AnimatedTextInput';
+import CheckButton from '../screens/CheckButton';
+import ForeCoffeeMenu from '../screens/ForeCoffee/ForeCoffeeMenu';
+import AnimatedRadioButton from '../screens/RadioButton/AnimatedRadioButton';
+import Slider from '../screens/Slider';
+import CompareImage from '../screens/SlidetoCompare';
+import SwitchButton from '../screens/SwitchButton';
 
 const SCREENS_LIST = [
-	{ screen: AnimateButton, screenRoute: 'AnimateButton', name: 'Animated Button' },
-	{ screen: AnimatedTextInput, screenRoute: 'AnimatedTextInput', name: 'Animated Text Input' },
-	{ screen: AnimatedRadioButton, screenRoute: 'AnimatedRadioButton', name: 'Animated Radio Button' },
-	{ screen: SwitchButton, screenRoute: 'SwitchButton', name: 'Animated Switch Button' },
-	{ screen: CheckButton, screenRoute: 'CheckButton', name: 'Animated Check Button' },
-	{ screen: Slider, screenRoute: 'Slider', name: 'Slider' },
-	{ screen: CompareImage, screenRoute: 'CompareImage', name: 'Compare Image' },
-	{ screen: MenuSection, screenRoute: 'Menu', name: 'Menu' },
-	// { screen: GojekBottomNav, screenRoute: 'GojekBottomNav', name: 'Gojek Bottom Navigation' }
-]
+  {
+    screen: AnimateButton,
+    screenRoute: 'AnimateButton',
+    name: 'Animated Button',
+  },
+  {
+    screen: AnimatedTextInput,
+    screenRoute: 'AnimatedTextInput',
+    name: 'Animated Text Input',
+  },
+  {
+    screen: AnimatedRadioButton,
+    screenRoute: 'AnimatedRadioButton',
+    name: 'Animated Radio Button',
+  },
+  {
+    screen: SwitchButton,
+    screenRoute: 'SwitchButton',
+    name: 'Animated Switch Button',
+  },
+  {
+    screen: CheckButton,
+    screenRoute: 'CheckButton',
+    name: 'Animated Check Button',
+  },
+  {screen: Slider, screenRoute: 'Slider', name: 'Slider'},
+  {screen: CompareImage, screenRoute: 'CompareImage', name: 'Compare Image'},
+  {
+    screen: ForeCoffeeMenu,
+    screenRoute: 'ForeCoffeeMenu',
+    name: 'Fore Coffee Menu',
+  },
+];
 
-export default SCREENS_LIST
+export default SCREENS_LIST;
---
+++ src/screens/ForeCoffee/ForeCoffeeMenu.js
@@ -0,0 +1,447 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  Pressable,
+  FlatList,
+  Dimensions,
+  Animated,
+} from 'react-native';
+import {useAnimatedRef} from 'react-native-reanimated';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const {width, height} = Dimensions.get('window');
+
+const colorTheme = {
+  primary: '#1d5027',
+  bg_secondary: '#fafafa',
+  bg_primary: '#ffffff',
+  grey: '#b2b2b2',
+  lightgrey: '#f5f5f5',
+};
+
+const colors20 = [];
+const colors5 = [];
+const foodCategories = [
+  {
+    name: '1 Lorem',
+    items: 3,
+  },
+  {
+    name: '2 Lorem',
+    items: 2,
+  },
+  {
+    name: '3 Lorem',
+    items: 5,
+  },
+  {
+    name: '4 Lorem',
+    items: 6,
+  },
+  {
+    name: '5 Lorem',
+    items: 3,
+  },
+  {
+    name: '6 Lorem',
+    items: 7,
+  },
+  {
+    name: '7 Lorem',
+    items: 4,
+  },
+  {
+    name: '8 Lorem',
+    items: 1,
+  },
+  {
+    name: '9 Lorem',
+    items: 6,
+  },
+  {
+    name: '10 Lorem',
+    items: 5,
+  },
+];
+for (let i = 0; i < 10; i++) {
+  colors20.push(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
+}
+
+for (let i = 0; i < 5; i++) {
+  colors5.push(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
+}
+
+const data = Object.keys(foodCategories).map(i => ({
+  id: i,
+  name: foodCategories[i].name,
+  items: foodCategories[i].items,
+  ref: React.createRef(),
+}));
+
+const ForeCoffeeMenu = () => {
+  const [scrollItemMeasures, setScrollItemMeasures] = React.useState([]);
+  const [activeMenu, setActiveMenu] = React.useState(0);
+  const rightScrollRef = useAnimatedRef();
+  const sideBarRef = React.useRef();
+
+  React.useEffect(() => {
+    const m = [];
+    data.forEach(food => {
+      food.ref.current.measureLayout(
+        rightScrollRef.current,
+        (x, y, width, height) => {
+          m.push({y, height});
+          if (m.length === data.length) {
+            setScrollItemMeasures(m);
+          }
+        },
+      );
+    });
+  }, []);
+
+  const scrollToHandler = i => {
+    rightScrollRef.current.scrollTo({
+      y: scrollItemMeasures[i].y,
+      animated: true,
+    });
+    setActiveMenu(i);
+    if (i * 65 - 45 / 2 > height / 4) {
+      sideBarRef?.current?.scrollToOffset({
+        offset: i * 65 - height / 4 + 115 / 2,
+        animated: true,
+      });
+    } else {
+      sideBarRef?.current?.scrollToOffset({
+        offset: 0,
+        animated: true,
+      });
+    }
+  };
+
+  return (
+    <View style={{flexDirection: 'row', flex: 1}}>
+      <SideBar
+        ref={sideBarRef}
+        active={activeMenu}
+        onPress={i => scrollToHandler(i)}
+      />
+      <View style={{flex: 1, backgroundColor: colorTheme.bg_primary}}>
+        <Header />
+        <ScrollView
+          ref={rightScrollRef}
+          contentContainerStyle={{padding: 20}}
+          showsVerticalScrollIndicator={false}
+          overScrollMode="never"
+          onScroll={ev => {
+            // console.log(
+            //   'set active ==> ',
+            //   Math.floor(ev.nativeEvent.contentOffset.y / height),
+            // );
+            // scrollToHandler(
+            //   Math.floor(ev.nativeEvent.contentOffset.y / height),
+            // );
+          }}>
+          <ImageScroll />
+          <FoodCategory data={data} />
+        </ScrollView>
+      </View>
+    </View>
+  );
+};
+
+export default ForeCoffeeMenu;
+
+const FoodCategory = ({data}) => {
+  return (
+    <>
+      {data.map((food, i) => (
+        <View key={i} ref={food.ref} style={{marginTop: 24}}>
+          <Text
+            style={{
+              fontWeight: 'bold',
+              color: colorTheme.primary,
+              fontSize: 20,
+              marginBottom: 8,
+            }}>
+            {food.name}
+          </Text>
+          <FoodGrid items={food.items} />
+        </View>
+      ))}
+    </>
+  );
+};
+
+const FoodGrid = ({items}) => {
+  const right = [];
+  const left = [];
+
+  for (let index = 0; index < items; index++) {
+    if (index % 2 === 0) {
+      right.push(<FoodItem key={index} />);
+    } else {
+      left.push(<FoodItem key={index} />);
+    }
+  }
+
+  return (
+    <View style={{flexDirection: 'row'}}>
+      <View style={{flex: 1}}>{right}</View>
+      <View style={{flex: 1, alignItems: 'flex-end'}}>{left}</View>
+    </View>
+  );
+};
+
+const FoodItem = () => {
+  return (
+    <View style={{width: 130, marginVertical: 8}}>
+      <View
+        style={{
+          aspectRatio: 1 / 1,
+          backgroundColor: colorTheme.grey,
+          borderRadius: 16,
+        }}
+      />
+      <View style={{paddingHorizontal: 8}}>
+        <Text
+          style={{
+            color: colorTheme.primary,
+            fontSize: 16,
+            fontWeight: 'bold',
+            marginTop: 8,
+          }}>
+          Lorem ipsum asd asd
+        </Text>
+        <Text
+          style={{
+            color: colorTheme.primary,
+            fontSize: 15,
+            marginTop: 8,
+          }}>
+          Rp. 10.000
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const Home = () => {
+  return (
+    <View
+      style={{
+        paddingVertical: 20,
+        paddingHorizontal: 24,
+        backgroundColor: colorTheme.primary,
+        borderTopRightRadius: 12,
+        borderBottomRightRadius: 12,
+      }}>
+      <View style={{alignItems: 'center'}}>
+        <Icon name="home" size={22} color="white" />
+        <Text
+          style={{
+            fontWeight: 'bold',
+            color: 'white',
+            fontSize: 11,
+            marginTop: 4,
+          }}>
+          Home
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const MenuItem = ({insideColor, text, onPress, active}) => {
+  return (
+    <Pressable
+      style={{alignItems: 'center', width: 50, marginVertical: 10}}
+      onPress={onPress}>
+      <View
+        style={{
+          width: 45,
+          height: 45,
+          backgroundColor: colorTheme.bg_primary,
+          borderRadius: 12,
+          alignItems: 'center',
+          justifyContent: 'center',
+          elevation: 2,
+          borderWidth: active + 1 === text ? 3 : 0,
+          borderColor: colorTheme.primary,
+        }}>
+        <View
+          style={{
+            backgroundColor: insideColor,
+            width: 20,
+            height: 20,
+            borderRadius: 6,
+          }}
+        />
+      </View>
+      <Text
+        style={{
+          fontWeight: 'bold',
+          color: colorTheme.primary,
+          fontSize: 11,
+          marginTop: 8,
+          textAlign: 'center',
+        }}>
+        {text}
+      </Text>
+    </Pressable>
+  );
+};
+
+const Header = () => {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        paddingVertical: 26,
+      }}>
+      <Text
+        style={{
+          color: colorTheme.primary,
+          fontSize: 22,
+          fontWeight: '500',
+        }}>
+        Menu
+      </Text>
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+        <Icon name="search" size={24} color={colorTheme.primary} />
+        <View style={{width: 16}} />
+        <Icon name="cart" size={24} color={colorTheme.grey} />
+      </View>
+    </View>
+  );
+};
+
+const SideBar = React.forwardRef(({onPress, active}, ref) => {
+  const renderItem = ({item, index}) => (
+    <MenuItem
+      insideColor={item}
+      text={index + 1}
+      active={active}
+      onPress={() => onPress(index)}
+    />
+  );
+
+  return (
+    <View>
+      <Home />
+      <FlatList
+        ref={ref}
+        style={{backgroundColor: colorTheme.bg_secondary}}
+        contentContainerStyle={{paddingVertical: 25, alignItems: 'center'}}
+        showsVerticalScrollIndicator={false}
+        overScrollMode="never"
+        data={colors20}
+        renderItem={renderItem}
+        keyExtractor={(_, index) => index}
+      />
+    </View>
+  );
+});
+
+const ImageScroll = () => {
+  const [measures, setMeasures] = React.useState([]);
+  const rowContainer = React.useRef();
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  const imageSize = width * 0.698;
+  const indicators = Object.keys(colors5).map(i => ({
+    ref: React.createRef(),
+  }));
+
+  React.useEffect(() => {
+    const m = [];
+    indicators.forEach(color => {
+      color.ref?.current?.measureLayout(
+        rowContainer.current,
+        (x, y, width, height) => {
+          m.push({x});
+          if (m.length === indicators.length) {
+            setMeasures(m);
+          }
+        },
+      );
+    });
+  }, []);
+
+  const renderItem = ({item}) => (
+    <View
+      style={{
+        width: imageSize,
+        backgroundColor: item,
+        aspectRatio: 1 / 1,
+        borderRadius: 16,
+      }}
+    />
+  );
+
+  const outputRange =
+    measures.length > 0 ? measures.map(measure => measure.x) : [0, 1, 2, 3, 4];
+
+  const translateX = scrollX.interpolate({
+    inputRange: indicators.map((_, i) => i * imageSize),
+    outputRange,
+    extrapolate: 'clamp',
+  });
+
+  return (
+    <View>
+      <View
+        style={{
+          aspectRatio: 1 / 1,
+        }}>
+        <Animated.FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={{flex: 1}}
+          pagingEnabled
+          decelerationRate="fast"
+          data={colors5}
+          renderItem={renderItem}
+          keyExtractor={(_, index) => index}
+          onScroll={Animated.event(
+            [{nativeEvent: {contentOffset: {x: scrollX}}}],
+            {useNativeDriver: true},
+          )}
+        />
+      </View>
+      <View
+        ref={rowContainer}
+        style={{flexDirection: 'row', alignItems: 'center', marginTop: 12}}>
+        {indicators.map((color, i) => (
+          <View
+            ref={color.ref}
+            key={i}
+            style={{
+              height: 4,
+              backgroundColor: colorTheme.lightgrey,
+              borderRadius: 50,
+              flex: 1,
+              marginHorizontal: 2,
+            }}
+          />
+        ))}
+        {measures.length > 0 && (
+          <Animated.View
+            style={{
+              position: 'absolute',
+              height: 4,
+              backgroundColor: colorTheme.primary,
+              borderRadius: 50,
+              width: 50,
+              left: 0,
+              transform: [{translateX}],
+            }}
+          />
+        )}
+      </View>
+    </View>
+  );
+};
