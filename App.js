6231b5678641073822188583b62ec4ae799e8cab
Drzaln <rizaaru@gmail.com>
2022-12-10 18:53:40 +0700

    upgrade to rn 0.70.6

--- App.js
+++ App.js
@@ -3,24 +3,27 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
 import SCREENS_LIST from './src/screenList';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerStyle: {elevation: 1}}}>
-        <Stack.Screen name="Home" component={Home} />
-        {SCREENS_LIST.map(item => (
-          <Stack.Screen
-            key={item.screenRoute}
-            name={item.screenRoute}
-            component={item.screen}
-            options={{headerTitle: item.name}}
-          />
-        ))}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerStyle: {elevation: 1}}}>
+          <Stack.Screen name="Home" component={Home} />
+          {SCREENS_LIST.map(item => (
+            <Stack.Screen
+              key={item.screenRoute}
+              name={item.screenRoute}
+              component={item.screen}
+              options={{headerTitle: item.name}}
+            />
+          ))}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
